7221575348cb87c9453ad947ba788f76629eb69b
Kevin Hawes <khawes187@gmail.com>
2021-06-30 14:00:44 -0400

    Simple Controller for Account

---
+++ student/dotnet/TenmoServer/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.DAO;
+using TenmoServer.Security;
+
+namespace TenmoServer.Controllers
+{
+    [Route("account/")]
+    [ApiController]   
+    public class AccountController : ControllerBase
+    {       
+        public class LoginController : ControllerBase
+        {
+            private readonly IAccountsDao accountsDao;         
+
+            public LoginController(IAccountsDao _accountsDao)
+            {               
+                accountsDao = _accountsDao;
+            }
+
+            [HttpGet("balance")]
+            public ActionResult<decimal> RetrieveAccountBalance(int userId)
+            {
+                               
+                return accountsDao.GetBalance(userId);         
+            }
+        }
+
+    }
+}
